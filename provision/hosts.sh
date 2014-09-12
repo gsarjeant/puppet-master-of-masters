a6934f56526ad8bd7cf6c365ac72a56dfb7bd9b2
Greg Sarjeant <gsarjeant@puppetlabs.com>
2014-09-12 06:49:03 -0700

    initial commit

---
+++ provision/hosts.sh
@@ -0,0 +1,4 @@
+echo "192.168.141.100 pe-mom.example.vm pe-mom" >> /etc/hosts
+echo "192.168.141.101 pe-master.tenant.example.vm pe-master" >> /etc/hosts
+echo "192.168.141.102 pe-puppetdb.tenant.example.vm pe-puppetdb" >> /etc/hosts
+echo "192.168.141.103 pe-console.tenant.example.vm pe-console" >> /etc/hosts
---
+++ provision/pe.sh
@@ -0,0 +1,31 @@
+# Shell provisioner script to install PE on a system,
+# using the PE installer and an answer file
+
+PE_INSTALLER_NAME=$1
+ANSWER_FILE_NAME=$2
+
+if [ "${PE_INSTALLER_NAME}x" == "x" ]
+then
+  echo "Please specify the PE installer filename"
+  exit 1
+fi
+
+if [ "${ANSWER_FILE_NAME}x" == "x" ]
+then
+  echo "Please specify the answer file name"
+  exit 1
+fi
+
+ANSWERS_SRC="/vagrant/answers/${ANSWER_FILE_NAME}"
+ANSWERS_DEST="/root/${ANSWER_FILE_NAME}"
+
+cp $ANSWERS_SRC $ANSWERS_DEST
+
+# Extract the PE installer
+PE_INSTALLER_SRC="/vagrant/${PE_INSTALLER_NAME}"
+PE_INSTALLER_DIR="/root/$(echo $PE_INSTALLER_NAME | sed -e 's/.tar.gz//')"
+
+# Install PE with the answer file
+cd /root
+tar -xvzf $PE_INSTALLER_SRC
+"${PE_INSTALLER_DIR}/puppet-enterprise-installer" -a ${ANSWERS_DEST}

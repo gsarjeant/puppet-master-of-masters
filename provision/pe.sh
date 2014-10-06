cf0ab381bbeb5d127ea58e926dc52312e01794c6
Greg Sarjeant <gsarjeant@puppetlabs.com>
2014-10-06 11:31:14 -0700

    Change MoM provisioning to use expected paths from RPMs

--- provision/pe.sh
+++ provision/pe.sh
@@ -1,9 +1,11 @@
 # Shell provisioner script to install PE on a system,
 # using the PE installer and an answer file
 
+# Input variables
 PE_INSTALLER_NAME=$1
 ANSWER_FILE_NAME=$2
 
+# Validation
 if [ "${PE_INSTALLER_NAME}x" == "x" ]
 then
   echo "Please specify the PE installer filename"
@@ -16,17 +18,30 @@ then
   exit 1
 fi
 
-#ANSWERS_SRC="/vagrant/answers/${ANSWER_FILE_NAME}"
-ANSWERS_SRC="/vagrant/bootstrap/answers/${ANSWER_FILE_NAME}.txt"
-ANSWERS_DEST="/root/${ANSWER_FILE_NAME}"
+PE_BASEDIR='/vagrant'
 
+# Mocking RPM installation of PE installer and answer files
+# vagrant location to pull PE tarball from (will be bundeled in RPM)
+PE_INSTALLER_SRC="${PE_BASEDIR}/${PE_INSTALLER_NAME}"
+# Directory to extract PE installer to (will be created by RPM)
+PE_INSTALLER_ROOT='/apps/pe-installer'
+# Directory to extract answer files to (will be created by RPM)
+PE_INSTALLER_ANSWERS_ROOT='/apps/pe-answers'
+# Directory that contains the PE installer after extraction
+PE_INSTALLER_DIR="${PE_INSTALLER_ROOT}/$(echo $PE_INSTALLER_NAME | sed -e 's/.tar.gz//')"
+
+# Create the PE installer directory and extract the installer
+# NOTE: The RPM is going to handle this
+mkdir -p $PE_INSTALLER_ROOT
+cd $PE_INSTALLER_ROOT
+tar -xvzf $PE_INSTALLER_SRC
+
+# Create the answer file directory and copy the answer file
+# NOTE: The answer file RPM will handle this
+ANSWERS_SRC="${PE_BASEDIR}/bootstrap/answers/${ANSWER_FILE_NAME}.txt"
+ANSWERS_DEST="${PE_INSTALLER_ANSWERS_ROOT}/${ANSWER_FILE_NAME}"
+mkdir -p $PE_INSTALLER_ANSWERS_ROOT
 cp $ANSWERS_SRC $ANSWERS_DEST
 
-# Extract the PE installer
-PE_INSTALLER_SRC="/vagrant/${PE_INSTALLER_NAME}"
-PE_INSTALLER_DIR="/root/$(echo $PE_INSTALLER_NAME | sed -e 's/.tar.gz//')"
-
 # Install PE with the answer file
-cd /root
-tar -xvzf $PE_INSTALLER_SRC
 "${PE_INSTALLER_DIR}/puppet-enterprise-installer" -a ${ANSWERS_DEST}

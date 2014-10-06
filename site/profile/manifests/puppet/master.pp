de4e47422b6056c27327e7bad5d946d2e594caee
Greg Sarjeant <gsarjeant@puppetlabs.com>
2014-10-06 13:16:50 -0700

    Convert site.pp file_line resources to static site.pp file resource

--- site/profile/manifests/puppet/master.pp
+++ site/profile/manifests/puppet/master.pp
@@ -40,18 +40,13 @@ class profile::puppet::master {
     activemq_brokers   => $profile::params::pe_activemq_brokers,
   }
 
+  ## Create site.pp from a static file
+  ## We don't have a good way to inject the role inclusion otherwise
   ## Set the stomp servers as a top-scope variable in site.pp
-  file_line { 'site_stomp_servers':
+  file { "${::settings::confdir}/manifests/site.pp":
     ensure => 'present',
-    line   => "\$fact_stomp_server = '${stomp_servers}'",
-    path   => "${::settings::confdir}/manifests/site.pp",
-  }
-
-  ## Include the class defined by the server_role fact in site.pp
-  file_line { 'site_include_server_role':
-    ensure => 'present',
-    line   => 'include $::server_role',
-    path   => "${::settings::confdir}/manifests/site.pp",
+    source => 'puppet:///modules/profile/etc/puppetlabs/puppet/manifests/site.pp,
+    notify => Service[ 'pe-httpd' ],
   }
 
   ## Configure r10k
@@ -74,14 +69,6 @@ class profile::puppet::master {
     ensure => 'directory',
     owner  => 'root',
     group  => 'root',
-  }
-
-  ## Create the external node classifier script
-  file { "${::settings::confdir}/external_nodes.rb":
-    ensure => 'file',
-    owner  => 'pe-puppet',
-    group  => 'pe-puppet',
-    mode   => '0750',
   }
 
   ## Configure Puppet's base module path - where can it find 'global' modules

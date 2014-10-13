aace0300650e50b8f3bb74de0918a7a22b92a884
Greg Sarjeant <gsarjeant@puppetlabs.com>
2014-10-13 14:23:11 -0500

    no more whitespace

--- site/profile/manifests/params.pp
+++ site/profile/manifests/params.pp
@@ -1,5 +1,4 @@
 class profile::params {
-  # Here's a change
   #############################################################################
   ## Global
   #############################################################################
@@ -21,7 +20,7 @@ class profile::params {
   $pe_master_ssl_private_key_dir      = "${pe_master_ssl_dir}/private_keys"
   $pe_console_share_dir               = '/opt/puppet/share/puppet-dashboard'
   $pe_console_internal_cert_dir       = "${pe_console_share_dir}/certs"
-  
+
   $pe_master_owner                    = 'pe-puppet'
   $pe_master_group                    = 'pe-puppet'
   $pe_console_owner                   = 'puppet-dashboard'
